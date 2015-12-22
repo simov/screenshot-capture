e01e0bdcf28575d7f54d57617059383cbb2d50c1
simov <simeonvelichkov@gmail.com>
2015-12-22 19:26:48 +0200

    Migrate to 2 space indentation

--- js/popup.js
+++ js/popup.js
@@ -1,34 +1,34 @@
 
 // chrome
 var c = {
-    send: function (message, data, done) {
-        data = data||{};
-        data.message = message;
-        chrome.extension.sendMessage(data, done);
-    },
-    storage: function (done) {
-        chrome.storage.sync.get(done);
-    }
+  send: function (message, data, done) {
+    data = data||{};
+    data.message = message;
+    chrome.extension.sendMessage(data, done);
+  },
+  storage: function (done) {
+    chrome.storage.sync.get(done);
+  }
 };
 
 $(function () {
-    var map = ['full', 'crop', 'wait'];
+  var map = ['full', 'crop', 'wait'];
 
-    c.storage(function (sync) {
-        $('#options input')
-            .eq(map.indexOf(sync.action))
-            .attr('checked', true);
+  c.storage(function (sync) {
+    $('#options input')
+      .eq(map.indexOf(sync.action))
+      .attr('checked', true);
 
-        if (sync.action != 'wait') return;
-        $('button').show();
-    });
+    if (sync.action != 'wait') return;
+    $('button').show();
+  });
 
-    $('#options label').on('click', function (e) {
-        var idx = $('#options label').index(this);
-        chrome.storage.sync.set({action:map[idx]});
-    });
+  $('#options label').on('click', function (e) {
+    var idx = $('#options label').index(this);
+    chrome.storage.sync.set({action:map[idx]});
+  });
 
-    $('button').on('click', function (e) {
-        c.send('save');
-    });
+  $('button').on('click', function (e) {
+    c.send('save');
+  });
 });
