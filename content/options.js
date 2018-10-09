c5cbef3622f1cc105864653311fa1bccf88e2cb0
simov <simeonvelichkov@gmail.com>
2018-10-09 21:08:03 +0300

    Migrate options page to mdc

--- content/options.js
+++ content/options.js
@@ -15,101 +15,109 @@ var state = {
 
 chrome.storage.sync.get((config) => {
   state.methods.forEach((item) => {
-    item.checked = (item.id === config.method)
+    item.checked = item.id === config.method
   })
   state.dpr.forEach((item) => {
-    item.checked = (item.id === config.dpr)
+    item.checked = item.id === config.dpr
   })
   m.redraw()
 })
 
 chrome.commands.getAll((commands) => {
-  var command = commands.filter((command) => command.name === 'take-screenshot')[0]
+  var command = commands.find((command) => command.name === 'take-screenshot')
   state.shortcut = command.shortcut
   m.redraw()
 })
 
 var events = {
   method: (item) => (e) => {
-    state.methods.forEach((item) => (item.checked = false))
+    state.methods.forEach((item) => item.checked = false)
     item.checked = true
     chrome.storage.sync.set({method: item.id})
   },
   dpr: (item) => (e) => {
-    state.dpr.forEach((item) => (item.checked = false))
+    state.dpr.forEach((item) => item.checked = false)
     item.checked = true
     chrome.storage.sync.set({dpr: item.id})
   }
 }
 
-function oncreate (vnode) {
-  componentHandler.upgradeElements(vnode.dom)
-}
-var onupdate = (type) => (vnode) => {
-  if (vnode.dom.classList.contains('is-checked') !== type.checked) {
-    vnode.dom.classList.toggle('is-checked')
+var onupdate = (item) => (vnode) => {
+  if (vnode.dom.classList.contains('active') !== item.checked) {
+    vnode.dom.classList.toggle('active')
   }
 }
 
 m.mount(document.querySelector('main'), {
-  view: () =>
-    m('.mdl-grid',
-      m('.mdl-cell mdl-cell--8-col-tablet mdl-cell--12-col-desktop',
-        m('.bs-callout',
-          m('h4', 'Capture Method',
-            m('span', 'Saved in ', m('code', 'PNG'), ' file format')
-          ),
-          state.methods.map((item) =>
-            m('.mdl-cell mdl-cell--8-col-tablet mdl-cell--12-col-desktop',
-              m('label.mdl-radio mdl-js-radio mdl-js-ripple-effect', {
-                oncreate, onupdate: onupdate(item)},
-                m('input[type=radio][name=method].mdl-radio__button', {
-                  checked: item.active ? 'checked' : null,
-                  onchange: events.method(item)
-                }),
-                m('span.mdl-radio__label', m('em', item.icon), item.title)
-              )
-            )
-          )
-        )
+  view: () => [
+    m('.bs-callout',
+      m('h4.mdc-typography--headline5', 'Capture Method',
+        m('span', 'Saved in ', m('code', 'PNG'), ' file format')
       ),
-
-      m('.mdl-cell mdl-cell--8-col-tablet mdl-cell--12-col-desktop',
-        m('.bs-callout',
-          m('h4', 'Screenshot Size',
-            m('span', 'Only for ', m('code', 'HDPI'), ' displays like Retina')
+      state.methods.map((item) =>
+        m('label.s-label', {onupdate: onupdate(item)},
+          m('.mdc-radio',
+            m('input.mdc-radio__native-control', {
+              type: 'radio', name: 'method',
+              checked: item.checked ? 'checked' : null,
+              onchange: events.method(item)
+            }),
+            m('.mdc-radio__background',
+              m('.mdc-radio__outer-circle'),
+              m('.mdc-radio__inner-circle'),
+            ),
           ),
-          state.dpr.map((item) =>
-            m('.mdl-cell mdl-cell--8-col-tablet mdl-cell--12-col-desktop',
-              m('label.mdl-radio mdl-js-radio mdl-js-ripple-effect', {
-                oncreate, onupdate: onupdate(item)},
-                m('input[type=radio][name=method].mdl-radio__button', {
-                  checked: item.active ? 'checked' : null,
-                  onchange: events.dpr(item)
-                }),
-                m('span.mdl-radio__label', item.title)
-              )
-            )
-          )
-        )
-      ),
-
-      m('.mdl-cell mdl-cell--8-col-tablet mdl-cell--12-col-desktop',
-        m('.bs-callout',
-          m('h4', 'Keyboard Shortcut',
-            (state.shortcut || null) &&
-            m('span', 'Press ', m('code', state.shortcut), ' to capture screenshot'),
-            (!state.shortcut || null) &&
-            m('span', 'Currently there is no keyboard shortcut set')
-          ),
-          m('.mdl-cell mdl-cell--8-col-tablet mdl-cell--12-col-desktop',
-            m('p', 'To set a keyboard shortcut:'),
-            m('p', '1. Navigate to ', m('code', 'chrome://extensions'),
-              ' and scroll down to the bottom of the page.'),
-            m('p', '2. Click on ', m('code', 'Keyboard shortcuts'),
-              ' and set a key combination for Screenshot Capture.')
-          )
+          m('span', m('em', item.icon), item.title)
         )
       )
+    ),
+
+    m('.bs-callout',
+      m('h4.mdc-typography--headline5', 'Screenshot Size',
+        m('span', 'Only for ', m('code', 'HDPI'), ' displays like Retina')
+      ),
+      state.dpr.map((item) =>
+        m('label.s-label', {onupdate: onupdate(item)},
+          m('.mdc-radio',
+            m('input.mdc-radio__native-control', {
+              type: 'radio', name: 'dpr',
+              checked: item.checked ? 'checked' : null,
+              onchange: events.dpr(item)
+            }),
+            m('.mdc-radio__background',
+              m('.mdc-radio__outer-circle'),
+              m('.mdc-radio__inner-circle'),
+            ),
+          ),
+          m('span', item.title)
+        )
+      )
+    ),
+
+    m('.bs-callout',
+      m('h4.mdc-typography--headline5', 'Keyboard Shortcut',
+        (state.shortcut || null) &&
+        m('span', 'Press ', m('code', state.shortcut), ' to capture screenshot'),
+        (!state.shortcut || null) &&
+        m('span', 'Currently there is no keyboard shortcut set')
+      ),
+      m('p', 'To set a keyboard shortcut:'),
+      m('p',
+        '1. Navigate to ',
+        m('code', 'chrome://extensions')
+      ),
+      m('p',
+        '2. Click on the menu icon ',
+        m('span.icon-menu'),
+        ' in the top left corner, and choose ',
+        m('code', 'Keyboard shortcuts'),
+        ' from the menu'
+      ),
+      m('p',
+        '3. Find Screenshot Capture and set key combination for the ',
+        m('code', 'Take Screenshot'),
+        ' action'
+      )
     )
+  ]
 })
