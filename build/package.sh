e9cea21448fb3c05947217b0323dde2776c28854
simov <simeonvelichkov@gmail.com>
2022-11-24 19:01:29 +0200

    Minor fixes

--- build/package.sh
+++ build/package.sh
@@ -7,13 +7,16 @@ browser=$1
 
 if [ -z "$browser" ]; then
   echo "Specify target browser"
-  echo "chrome, firefox, edge"
+  echo "chrome, firefox"
   exit 1
 fi
 
 # set current working directory to directory of the shell script
 cd "$(dirname "$0")"
 
+# cleanup
+rm -rf ../vendor
+rm ../screenshot-capture.zip
 mkdir -p ../vendor
 
 # build deps
@@ -21,30 +24,31 @@ sh jquery/build.sh
 sh mdc/build.sh
 sh mithril/build.sh
 
-# archive
+# copy files
 mkdir -p tmp
 mkdir -p tmp/screenshot-capture
 cd ..
-cp -r background content icons options vendor build/tmp/screenshot-capture/
+cp -r background content icons options vendor LICENSE build/tmp/screenshot-capture/
 
 if [ "$browser" = "chrome" ]; then
-  cp manifest.json build/tmp/screenshot-capture/
+  cp manifest.chrome.json build/tmp/screenshot-capture/manifest.json
+  cp manifest.chrome.json manifest.json
 elif [ "$browser" = "firefox" ]; then
   cp manifest.firefox.json build/tmp/screenshot-capture/manifest.json
-elif [ "$browser" = "edge" ]; then
-  cp manifest.edge.json build/tmp/screenshot-capture/manifest.json
+  cp manifest.firefox.json manifest.json
 fi
 
-# zip the screenshot-capture folder itself
-if [ "$browser" = "chrome" ] || [ "$browser" = "edge" ]; then
+# archive the screenshot-capture folder itself
+if [ "$browser" = "chrome" ]; then
   cd build/tmp/
   zip -r ../../screenshot-capture.zip screenshot-capture
   cd ..
-# zip the contents of the screenshot-capture folder
+# archive the contents of the screenshot-capture folder
 elif [ "$browser" = "firefox" ]; then
   cd build/tmp/screenshot-capture/
   zip -r ../../../screenshot-capture.zip .
   cd ../../
 fi
 
+# cleanup
 rm -rf tmp/
